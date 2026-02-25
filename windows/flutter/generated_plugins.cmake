0373a5bfad5968f52338873213c352a3b9abfc55
KLGIT886 <65105731+KLGIT886@users.noreply.github.com>
2026-02-25 13:54:51 +0800

    更详细的提交信息

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,9 +6,8 @@ list(APPEND FLUTTER_PLUGIN_LIST
   media_kit_libs_windows_video
   media_kit_video
   screen_brightness_windows
-  screen_retriever
+  screen_retriever_windows
   url_launcher_windows
-  volume_controller
   window_manager
 )
 
